d4cbe4f9b5bb3c8ab884bd265a0d62b8ff339b34
wengxiaojun <xiaojun@smartx.com>
2022-02-14 14:03:54 +0800

    update parameters.json

--- cloudtower-api-doc/swagger/components/Download.tsx
+++ cloudtower-api-doc/swagger/components/Download.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useDocsVersion  } from '@docusaurus/theme-common';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import i18next from '../i18n';
 import { specMap } from '../utils';
-import { useDocsVersion  } from '@docusaurus/theme-common';
+
 
 const Download = () => {
   const { version} = useDocsVersion();
+  const { i18n } = useDocusaurusContext();
   const swaggerSpec = version && specMap[version] ? specMap[version] : specMap[Object.keys(specMap)[0]];
   const href = "data:text/plain;charset=utf-8," + encodeURIComponent(JSON.stringify(swaggerSpec, null, 2));
   const name = `cloudtower-api-${swaggerSpec.info.version}`;
   return (
-    <a href={href} download={`${name}.json`}>{i18next.t('components.download')}</a>
+    <a href={href} download={`${name}.json`}>{i18next.t('components.download', {
+      lng: i18n.currentLocale,
+    })}</a>
   )
 }
 
