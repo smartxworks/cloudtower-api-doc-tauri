42a1ed7a7f12385dad778ced898883f580320f70
wengxiaojun <xiaojun@smartx.com>
2022-03-16 15:44:07 +0800

    add examples

--- cloudtower-api-doc/swagger/examples/index.ts
+++ cloudtower-api-doc/swagger/examples/index.ts
@@ -3,6 +3,14 @@ import { SupportLanguage } from '../utils';
 import { getUserRoleNexts } from "./getUserRoleNexts";
 import { getUserRoleNextsConnection } from './getUserRoleNextsConnection';
 import { getCreateUserRoleNexts} from './getCreateUserRoleNexts';
+import { getUpdateUserRoleNexts } from './getUpdateUserRoleNexts';
+import { getDeleteUserRoleNexts } from './getDeleteUserRoleNexts';
+import { getUsers } from './getUsers';
+import { getUsersConnection } from './getUsersConnection';
+import { getLogin } from './getLogin';
+import { getCreateUser} from './getCreateUser';
+import { getDeleteUser } from './getDeleteUser';
+import { getUpdateUsers } from './getUpdateUsers';
 
 
 export const getExamples = (api: SwaggerApi, lng: SupportLanguage ) => {
@@ -13,7 +21,23 @@ export const getExamples = (api: SwaggerApi, lng: SupportLanguage ) => {
       return getUserRoleNextsConnection(lng);
     case '/create-role':
       return getCreateUserRoleNexts(lng);
+    case '/update-role':
+      return getUpdateUserRoleNexts(lng);
+    case '/delete-role':
+      return getDeleteUserRoleNexts(lng);
+    case '/get-users':
+      return getUsers(lng)
+    case '/get-users-connection':
+      return getUsersConnection(lng)
+    case '/login':
+      return getLogin(lng)
+    case '/create-user':
+      return getCreateUser(lng);
+    case '/delete-user':
+      return getDeleteUser(lng)
+    case '/update-user':
+      return getUpdateUsers(lng)
     default:
-      return null;
+      return undefined;
   }
 };
