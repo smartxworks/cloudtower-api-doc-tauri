b72eb41a4fd7c7b15f0a236a27c5cc91fd13dd9b
wengxiaojun <xiaojun@smartx.com>
2022-03-19 15:51:04 +0800

    refactor examples

--- cloudtower-api-doc/swagger/examples/common.ts
+++
@@ -1,4 +0,0 @@
-import { SupportLanguage } from "../utils";
-import { OpenAPIV3 } from 'openapi-types';
-
-export type GetExamplesFunc = (lng: SupportLanguage) => Record<string,  OpenAPIV3.ExampleObject> 
