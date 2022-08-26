0e01368a4f2bf997eec02519b76a22c28f3d723d
wengxiaojun <xiaojun@smartx.com>
2022-08-26 10:46:57 +0800

    update tauri build config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,23 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::{utils::config::AppUrl, window::WindowBuilder, WindowUrl};
 
 fn main() {
+  let port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+  let mut context = tauri::generate_context!();
+  let url = format!("http://localhost:{}", port).parse().unwrap();
+  let window_url = WindowUrl::External(url);
+  // rewrite the config so the IPC is enabled on this URL
+  context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+  context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
+    .plugin(tauri_plugin_localhost::Builder::new(port).build())
+    .setup(move |app| {
+      WindowBuilder::new(app, "second".to_string(), window_url)
+        .title("CloudTower API Doc")
+        .build()?;
+      Ok(())
+    })
+    .run(context)
     .expect("error while running tauri application");
 }
