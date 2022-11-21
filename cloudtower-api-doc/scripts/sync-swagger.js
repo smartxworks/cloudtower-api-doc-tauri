ee362179840696a2134d2a02c2a4d349d9be7d52
wengxiaojun <xiaojun@smartx.com>
2022-11-21 13:52:35 +0800

    feat: update scripts for sync api

--- cloudtower-api-doc/scripts/sync-swagger.js
+++ cloudtower-api-doc/scripts/sync-swagger.js
@@ -1,31 +1,26 @@
+#!/usr/bin/env node
 const fs = require("fs");
-const nodePath = require("path");
+const url = require('url');
+const http = require('http');
+const path = require('path');
+const versions = require('../versions.json')
 
-const { Gitlab } = require("@gitbeaker/node");
-
-const Api = new Gitlab({
-  token: process.env.GITLAB_TOKEN,
-  host: "http://gitlab.smartx.com/",
-  version: 4,
-});
-
-const SDK_DIR = 'sdk-schema'
-const SWAGGER_DIFF = 454;
-const sync = async () => {
-  const dir = await Api.Repositories.tree(SWAGGER_DIFF, {
-    ref: 'master',
-    path: SDK_DIR
-  });
-  for(const { path, name } of dir) {
-    const result = await Api.RepositoryFiles.show(
-      SWAGGER_DIFF,
-      path,
-      "master"
-    );
-    const fileName = `v${name.replace('.json', '')}-swagger.json`;
-    const file = Buffer.from(result.content, result.encoding).toString("utf-8");
-    const filePath = nodePath.resolve(__dirname, `../swagger/specs/${fileName}`)
-    fs.writeFileSync(filePath, file);
+const SCHEMA_URL = `http://192.168.28.9/schema/v3/`
+const sync = () => {
+  for(const v of versions) {
+    const fileUrl = new url.URL(v.replace('v', ''), SCHEMA_URL);
+    const fileName = `${v}-swagger.json`;
+    const filePath = path.resolve(__dirname, `../swagger/specs/${fileName}`)
+    http.get(fileUrl.href, function(response) {
+      let content = '';
+      response.on('data', (chunk) => content += chunk);
+      response.on('end', () => {
+        if(response.statusCode === 200) {
+          const formatContent = JSON.stringify(JSON.parse(content), null, 2)
+          fs.writeFileSync(filePath, formatContent, 'utf-8')
+        }
+      })
+    });
   }
 };
 sync();
