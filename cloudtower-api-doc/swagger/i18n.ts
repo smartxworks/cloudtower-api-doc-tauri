960cda77c4e137638b68db296582723e83d8e796
wengxiaojun <xiaojun@smartx.com>
2022-11-21 15:55:02 +0800

    update to v2.4.0

--- cloudtower-api-doc/swagger/i18n.ts
+++ cloudtower-api-doc/swagger/i18n.ts
@@ -26,6 +26,9 @@ import en2_2API from './locales/en/v2.2.0.json';
 import zh2_3API from './locales/zh/v2.3.0.json';
 import en2_3API from './locales/en/v2.3.0.json';
 
+import zh2_4API from './locales/zh/v2.4.0.json';
+import en2_4API from './locales/en/v2.4.0.json';
+
 
 export type ApiDoc = {
   summary: string;
@@ -44,6 +47,7 @@ i18next.init({
         ['v2_1_0']: en2_1API,
         ['v2_2_0']: en2_2API,
         ['v2_3_0']: en2_3API,
+        ['v2_4_0']: en2_4API,
         components: enComponents,
     },
     [SupportLanguage.zh]: {
@@ -54,20 +58,21 @@ i18next.init({
         ['v2_1_0']: zh2_1API,
         ['v2_2_0']: zh2_2API,
         ['v2_3_0']: zh2_3API,
+        ['v2_4_0']: zh2_4API,
         components: zhComponents,
     },
   },
   lng: SupportLanguage.zh,
   updateMissing: true,
   fallbackLng: [SupportLanguage.en, SupportLanguage.zh],
-  fallbackNS: ['v1_8_0','v1_9_0','v1_10_0', 'v2_0_0', 'v2_1_0','v2_2_0', 'v2_3_0'],
+  fallbackNS: ['v1_8_0','v1_9_0','v1_10_0', 'v2_0_0', 'v2_1_0','v2_2_0', 'v2_3_0', 'v2_4_0'],
   interpolation: {
     prefix: "{",
     suffix: "}",
     escapeValue: false,
   },
   keySeparator: false,
-  ns: ['v1_8_0', 'v1_9_0', 'v1_10_0','v2_0_0', 'v2_1_0', 'v2_2_0', 'v2_3_0', 'components'],
+  ns: ['v1_8_0', 'v1_9_0', 'v1_10_0','v2_0_0', 'v2_1_0', 'v2_2_0', 'v2_3_0', 'v2_4_0','components'],
   nsSeparator: ".",
   load: "currentOnly",
   react: {
