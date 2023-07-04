5f6c3bd0c7e6e43544891180fed7dc83ca54e717
wengxiaojun <xiaojun@smartx.com>
2023-07-04 18:33:28 +0800

    feat:uploda download page

--- cloudtower-api-doc/swagger/components/Download.tsx
+++ cloudtower-api-doc/swagger/components/Download.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import { useActiveVersion } from '@docusaurus/plugin-content-docs/client';
 import i18next from '../i18n';
 import { specMap } from '../utils';
 
 
-const Download = () => {
+const Download = (props: {
+  version: string
+}) => {
   const { i18n } = useDocusaurusContext();
-  const activeVersion = useActiveVersion();
-  const version = activeVersion.name;
+  const { version } = props;
   const swaggerSpec = version && specMap[version] ? specMap[version] : specMap[Object.keys(specMap)[0]];
   const href = "data:text/plain;charset=utf-8," + encodeURIComponent(JSON.stringify(swaggerSpec, null, 2));
   const name = `cloudtower-api-${swaggerSpec.info.version}`;
   return (
-    <a href={href} download={`${name}.json`}>{i18next.t('components.download', {
-      lng: i18n.currentLocale,
-    })}</a>
+    <a href={href} download={`${name}.json`}>Swagger Json</a>
   )
 }
 
