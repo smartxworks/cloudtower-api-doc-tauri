31532f6cbcfd3d8ef5e6d0b88049be3f07c16248
estella <estella@estelladeMacBook-Pro.local>
2024-04-09 10:47:46 +0800

    add 4.2 branch

--- cloudtower-api-doc/swagger/i18n.ts
+++ cloudtower-api-doc/swagger/i18n.ts
@@ -65,8 +65,11 @@ import en4_0API from './locales/en/4.0.0.json';
 import zh4_1API from './locales/zh/4.1.0.json';
 import en4_1API from './locales/en/4.1.0.json';
 
+import zh4_2API from './locales/zh/4.2.0.json';
+import en4_2API from './locales/en/4.2.0.json';
 
-export const fallbackNS = ['4_1_0','4_0_0','3_4_0','3_3_0', '3_2_0', '3_1_0', '3_0_0', '2_8_0', '2_7_0', '2_6_0', '2_5_0', '2_4_0', '2_3_0', '2_2_0', '2_1_0', '2_0_0', '1_10_0', '1_9_0', '1_8_0'];
+
+export const fallbackNS = ['4_2_0','4_1_0','4_0_0','3_4_0','3_3_0', '3_2_0', '3_1_0', '3_0_0', '2_8_0', '2_7_0', '2_6_0', '2_5_0', '2_4_0', '2_3_0', '2_2_0', '2_1_0', '2_0_0', '1_10_0', '1_9_0', '1_8_0'];
 export type ApiDoc = {
   summary: string;
   description: string;
@@ -96,6 +99,7 @@ i18next.init({
         ['3_4_0']: en3_4API,
         ['4_0_0']: en4_0API,
         ['4_1_0']: en4_1API,
+        ['4_2_0']: en4_2API,
         components: enComponents,
     },
     [SupportLanguage.zh]: {
@@ -118,6 +122,7 @@ i18next.init({
         ['3_4_0']: zh3_4API,
         ['4_0_0']: zh4_0API,
         ['4_1_0']: zh4_1API,
+        ['4_2_0']: zh4_2API,
         components: zhComponents,
     },
   },
@@ -131,7 +136,7 @@ i18next.init({
     escapeValue: false,
   },
   keySeparator: false,
-  ns: ['1_8_0', '1_9_0', '1_10_0','2_0_0', '2_1_0', '2_2_0', '2_3_0', '2_4_0', '2_5_0','2_6_0', '2_7_0','2_8_0', '3_0_0', '3_1_0','3_2_0', '3_3_0', '3_4_0', '4_0_0', '4_1_0','components'],
+  ns: ['1_8_0', '1_9_0', '1_10_0','2_0_0', '2_1_0', '2_2_0', '2_3_0', '2_4_0', '2_5_0','2_6_0', '2_7_0','2_8_0', '3_0_0', '3_1_0','3_2_0', '3_3_0', '3_4_0', '4_0_0', '4_1_0','4_2_0','components'],
   nsSeparator: ".",
   load: "currentOnly",
   react: {
