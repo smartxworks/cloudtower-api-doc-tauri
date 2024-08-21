1e2db1df9efc5f348c0fe036d0cbe82e2d9e4e03
shenwen.yin <shenwen.yin@smartx.com>
2024-08-21 05:51:37 +0000

    chore: update to v4.4.0 schema

--- cloudtower-api-doc/swagger/i18n.ts
+++ cloudtower-api-doc/swagger/i18n.ts
@@ -1,63 +1,59 @@
 import i18next from "i18next";
-import { OpenAPIV3 } from 'openapi-types';
+import { OpenAPIV3 } from "openapi-types";
 import { SupportLanguage } from "./utils";
-import enComponents from './locales/en/components.json';
-import zhComponents from './locales/zh/components.json';
+import enComponents from "./locales/en/components.json";
+import zhComponents from "./locales/zh/components.json";
 
-import zh1_8Api from './locales/zh/1.8.0.json';
-import en1_8API from './locales/en/1.8.0.json'
+import zh1_8Api from "./locales/zh/1.8.0.json";
+import en1_8API from "./locales/en/1.8.0.json";
 
-import zh1_9Api from './locales/zh/1.9.0.json';
-import en1_9API from './locales/en/1.9.0.json'
+import zh1_9Api from "./locales/zh/1.9.0.json";
+import en1_9API from "./locales/en/1.9.0.json";
 
-import zh1_10Api from './locales/zh/1.10.0.json';
-import en1_10Api from './locales/en/1.10.0.json'
+import zh1_10Api from "./locales/zh/1.10.0.json";
+import en1_10Api from "./locales/en/1.10.0.json";
 
-import zh2_0API from './locales/zh/2.0.0.json';
-import en2_0API from './locales/en/2.0.0.json';
+import zh2_0API from "./locales/zh/2.0.0.json";
+import en2_0API from "./locales/en/2.0.0.json";
 
-import zh2_1API from './locales/zh/2.1.0.json';
-import en2_1API from './locales/en/2.1.0.json';
+import zh2_1API from "./locales/zh/2.1.0.json";
+import en2_1API from "./locales/en/2.1.0.json";
 
+import zh2_2API from "./locales/zh/2.2.0.json";
+import en2_2API from "./locales/en/2.2.0.json";
 
-import zh2_2API from './locales/zh/2.2.0.json';
-import en2_2API from './locales/en/2.2.0.json';
+import zh2_3API from "./locales/zh/2.3.0.json";
+import en2_3API from "./locales/en/2.3.0.json";
 
-import zh2_3API from './locales/zh/2.3.0.json';
-import en2_3API from './locales/en/2.3.0.json';
+import zh2_4API from "./locales/zh/2.4.0.json";
+import en2_4API from "./locales/en/2.4.0.json";
 
-import zh2_4API from './locales/zh/2.4.0.json';
-import en2_4API from './locales/en/2.4.0.json';
+import zh2_5API from "./locales/zh/2.5.0.json";
+import en2_5API from "./locales/en/2.5.0.json";
 
+import zh2_6API from "./locales/zh/2.6.0.json";
+import en2_6API from "./locales/en/2.6.0.json";
 
-import zh2_5API from './locales/zh/2.5.0.json';
-import en2_5API from './locales/en/2.5.0.json';
+import zh2_7API from "./locales/zh/2.7.0.json";
+import en2_7API from "./locales/en/2.7.0.json";
 
-import zh2_6API from './locales/zh/2.6.0.json';
-import en2_6API from './locales/en/2.6.0.json';
+import zh2_8API from "./locales/zh/2.8.0.json";
+import en2_8API from "./locales/en/2.8.0.json";
 
+import zh3_0API from "./locales/zh/3.0.0.json";
+import en3_0API from "./locales/en/3.0.0.json";
 
-import zh2_7API from './locales/zh/2.7.0.json';
-import en2_7API from './locales/en/2.7.0.json';
+import zh3_1API from "./locales/zh/3.1.0.json";
+import en3_1API from "./locales/en/3.1.0.json";
 
-import zh2_8API from './locales/zh/2.8.0.json';
-import en2_8API from './locales/en/2.8.0.json';
+import zh3_2API from "./locales/zh/3.2.0.json";
+import en3_2API from "./locales/en/3.2.0.json";
 
-import zh3_0API from './locales/zh/3.0.0.json';
-import en3_0API from './locales/en/3.0.0.json';
+import zh3_3API from "./locales/zh/3.3.0.json";
+import en3_3API from "./locales/en/3.3.0.json";
 
-import zh3_1API from './locales/zh/3.1.0.json';
-import en3_1API from './locales/en/3.1.0.json';
-
-import zh3_2API from './locales/zh/3.2.0.json';
-import en3_2API from './locales/en/3.2.0.json';
-
-
-import zh3_3API from './locales/zh/3.3.0.json';
-import en3_3API from './locales/en/3.3.0.json';
-
-import zh3_4API from './locales/zh/3.4.0.json';
-import en3_4API from './locales/en/3.4.0.json';
+import zh3_4API from "./locales/zh/3.4.0.json";
+import en3_4API from "./locales/en/3.4.0.json";
 
 import zh3_4_4API from './locales/zh/3.4.4.json';
 import en3_4_4API from './locales/en/3.4.4.json';
@@ -74,7 +70,34 @@ import en4_2API from './locales/en/4.2.0.json';
 import zh4_3API from './locales/zh/4.3.0.json';
 import en4_3API from './locales/en/4.3.0.json';
 
-export const fallbackNS = ['4_3_0','4_2_0','4_1_0','4_0_0','3_4_4', '3_4_0','3_3_0', '3_2_0', '3_1_0', '3_0_0', '2_8_0', '2_7_0', '2_6_0', '2_5_0', '2_4_0', '2_3_0', '2_2_0', '2_1_0', '2_0_0', '1_10_0', '1_9_0', '1_8_0'];
+import zh4_4API from "./locales/zh/4.4.0.json";
+import en4_4API from "./locales/en/4.4.0.json";
+
+export const fallbackNS = [
+  "4_4_0",
+  "4_3_0",
+  "4_2_0",
+  "4_1_0",
+  "4_0_0",
+  "3_4_4",
+  "3_4_0",
+  "3_3_0",
+  "3_2_0",
+  "3_1_0",
+  "3_0_0",
+  "2_8_0",
+  "2_7_0",
+  "2_6_0",
+  "2_5_0",
+  "2_4_0",
+  "2_3_0",
+  "2_2_0",
+  "2_1_0",
+  "2_0_0",
+  "1_10_0",
+  "1_9_0",
+  "1_8_0",
+];
 export type ApiDoc = {
   summary: string;
   description: string;
@@ -107,6 +130,7 @@ i18next.init({
         ['4_1_0']: en4_1API,
         ['4_2_0']: en4_2API,
         ['4_3_0']: en4_3API,
+        ["4_4_0"]: en4_4API,
         components: enComponents,
     },
     [SupportLanguage.zh]: {
@@ -132,6 +156,7 @@ i18next.init({
         ['4_1_0']: zh4_1API,
         ['4_2_0']: zh4_2API,
         ['4_3_0']: zh4_3API,
+        ["4_4_0"]: zh4_4API,
         components: zhComponents,
     },
   },
@@ -145,13 +170,12 @@ i18next.init({
     escapeValue: false,
   },
   keySeparator: false,
-  ns: ['1_8_0', '1_9_0', '1_10_0','2_0_0', '2_1_0', '2_2_0', '2_3_0', '2_4_0', '2_5_0','2_6_0', '2_7_0','2_8_0', '3_0_0', '3_1_0','3_2_0', '3_3_0', '3_4_0', '3_4_4', '4_0_0', '4_1_0', '4_2_0', '4_3_0','components'],
+  ns: ['1_8_0', '1_9_0', '1_10_0','2_0_0', '2_1_0', '2_2_0', '2_3_0', '2_4_0', '2_5_0','2_6_0', '2_7_0','2_8_0', '3_0_0', '3_1_0','3_2_0', '3_3_0', '3_4_0', '3_4_4', '4_0_0', '4_1_0', '4_2_0', '4_3_0', '4_4_0','components'],
   nsSeparator: ".",
   load: "currentOnly",
   react: {
     bindI18n: "languageChanged addResource",
   },
 });
-
 
 export default i18next;
