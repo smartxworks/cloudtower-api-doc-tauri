64f74d00e220d62a37fe2c035379be37ac33941c
wengxiaojun <xiaojun@smartx.com>
2025-12-16 17:06:49 +0800

    refactor: update menu styles and TOC component to enhance usability and appearance

--- cloudtower-api-doc/src/theme/TOC/index.tsx
+++ cloudtower-api-doc/src/theme/TOC/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import TOCItems from '@theme/TOCItems';
 import type {Props} from '@theme/TOC';
-
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 
 // Using a custom className
@@ -10,9 +10,18 @@ import styles from './styles.module.css';
 const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
 const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
 
+interface TOCConfig {
+  title?:string;
+}
 export default function TOC({className, ...props}: Props): JSX.Element {
+  const { siteConfig: { customFields }} = useDocusaurusContext();
+
   return (
     <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+      {
+        (customFields.toc as TOCConfig)?.title ?
+        <span className="table-of-contents__title">{(customFields.toc as TOCConfig).title}</span> : null
+      }
       <TOCItems
         {...props}
         linkClassName={LINK_CLASS_NAME}
