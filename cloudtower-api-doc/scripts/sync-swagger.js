2cdfe5cd85484016cd1e89b9dc833d3f698d6913
wengxiaojun <xiaojun@smartx.com>
2026-01-15 14:49:19 +0800

    refine workflow and tranlate docs

--- cloudtower-api-doc/scripts/sync-swagger.js
+++ cloudtower-api-doc/scripts/sync-swagger.js
@@ -6,9 +6,9 @@ const path = require('path');
 const { program } = require('commander');
 
 
-const SCHEMA_URL = `http://tower.smartx.com/v2/api/swagger3`
-const sync = (v) => {
-  const fileUrl = new url.URL(SCHEMA_URL);
+const SCHEMA_URL = `http://file.smtx.io/operation-api-schema/smartx`
+const sync = (t, v) => {
+  const fileUrl = new url.URL(`${SCHEMA_URL}/${t}/swagger-3.0.json`);
   const fileName = `${v}-swagger.json`;
   const filePath = path.resolve(__dirname, `../static/specs/${fileName}`)
   http.get(fileUrl.href, function(response) {
@@ -25,7 +25,8 @@ const sync = (v) => {
 
 
 program
+  .requiredOption('-t --oapi-tag <char>')
   .requiredOption('-v --version <char>')
 program.parse();
 
-sync(program.opts().version);
+sync(program.opts().oapiTag, program.opts().version);
